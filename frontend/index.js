159fb8b96e334fc736c9a0f5645c5f14a98897ed
MortyOW <adam.simon@edu.devinci.fr>
2023-01-25 17:09:25 +0100

    :bug: FIX CONFLICT

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,14 @@
 const socket = io('http://127.0.0.1:3000')
+const fetch = document.getElementById('fetch')
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+
+fetch.addEventListener('click', () => {
+    fetch('http://127.0.0.1:3000/api/user', {
+        method: 'GET',
+    }).then(response => response.json)
+        .then(data => {
+            name.innerText = `${data.lastname} ${data.firstname}`
+            email.innerText = `${data.email}`
+        })
+})
